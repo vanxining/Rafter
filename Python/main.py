8630ba01aa8a5f6364f72790ae84ca0b34972080
vanxining <vanxining@gmail.com>
2022-05-02 16:35:01 +0800

    AppendEntries RPC.

--- Python/main.py
+++ Python/main.py
@@ -1,14 +1,18 @@
 import asyncio
 import json
+import logging
 import random
 import sys
-import traceback
 
 from dataclasses import dataclass
 from datetime import datetime
 
 from dataclass_wizard import asdict, fromdict
 
+import util
+
+
+LOGGER = logging.getLogger("rafter")
 
 EPOCH = datetime.fromtimestamp(0)
 LOOP: asyncio.AbstractEventLoop
@@ -23,23 +27,23 @@ class RequestVote:
 
 
 @dataclass
+class Log:
+    index: int
+    term: int
+
+
+@dataclass
 class AppendEntries:
     term: int
 
     leader_addr: str
     leader_commit_index: int
 
-    entries: list
+    entries: list[Log]
 
     # For validation
     prev_log_index: int
     prev_log_term: int
-
-
-@dataclass
-class Log:
-    index: int
-    term: int
 
 
 PONG = b'{"ok":true,"result":"PONG"}'
@@ -59,12 +63,9 @@ class Peer(object):
         try:
             self.reader, self.writer = await asyncio.open_connection(self.host, self.port)
             self.last_active_time = datetime.now()
-
             return True
         except Exception:
-            print(f"Failed to connect to peer at {self.addr}")
-            traceback.print_exc()
-
+            LOGGER.exception(f"Failed to connect to peer at {self.addr}")
             return False
 
     def is_connected(self):
@@ -90,8 +91,7 @@ class Peer(object):
             else:
                 return await self.initialize()
         except Exception:
-            print(f"Failed to ping peer at {self.addr}")
-            traceback.print_exc()
+            LOGGER.exception(f"Failed to ping peer at {self.addr}")
 
         return False
 
@@ -101,13 +101,12 @@ class Peer(object):
 
         try:
             response = await self.send(b"LOOK_FOR_LEADER")
-            if response and response != FAILURE:
+            if response and response.rstrip() != FAILURE:
                 obj = json.loads(response)
                 if obj.get("ok") is True:
                     return obj.get("value")
         except Exception:
-            print(f"Failed to query peer at {self.addr} for Leader")
-            traceback.print_exc()
+            LOGGER.exception(f"Failed to query peer at {self.addr} for Leader")
 
         return None
 
@@ -116,14 +115,13 @@ class Peer(object):
             return False
 
         try:
-            response = await self.send(f"REQUEST_VOTE {json.dumps(asdict(rpc))}")
-            if response and response != FAILURE:
+            response = await self.send(f"REQUEST_VOTE {util.dump_as_json(asdict(rpc))}")
+            if response and response.rstrip() != FAILURE:
                 obj = json.loads(response)
                 if obj.get("ok") is True and obj.get("term") == rpc.term:
                     return True
         except Exception:
-            print(f"Failed to send RequestVote RPC to peer at {self.addr}")
-            traceback.print_exc()
+            LOGGER.exception(f"Failed to send RequestVote RPC to peer at {self.addr}")
 
         return False
 
@@ -145,8 +143,8 @@ class Node(object):
         self.leader_addr = None
 
         # Only for Leader
-        self.next_indices = {}
-        self.match_indices = {}
+        self.next_indices = []
+        self.match_indices = []
 
         # Cluster status
         self.peers = [Peer(addr) for addr in peers]
@@ -156,13 +154,13 @@ class Node(object):
         for index, result in enumerate(results):
             connected, leader_addr = result
             if connected is True:
-                print(f"Connected to peer at {self.peers[index].addr}")
+                LOGGER.info(f"Connected to peer at {self.peers[index].addr}")
                 if leader_addr is not None:
                     if self.leader_addr is not None and self.leader_addr != leader_addr:
                         if self.leader_addr != leader_addr:
                             raise RuntimeError("More than one Leader in the cluster!")
                     else:
-                        print(f"Get Leader at {leader_addr}")
+                        LOGGER.info(f"Get Leader at {leader_addr}")
                     self.leader_addr = leader_addr
 
     @staticmethod
@@ -177,7 +175,13 @@ class Node(object):
         results = await asyncio.gather(*[peer.ping() for peer in self.peers], return_exceptions=True)
         for index, result in enumerate(results):
             if not result:
-                print(f"Can't ping peer at {self.peers[index].addr}")
+                LOGGER.info(f"Can't ping peer at {self.peers[index].addr}")
+
+    def on_look_for_leader(self) -> bytes:
+        if self.leader_addr is not None:
+            return b'{"ok":true,"value":"%s"}' % self.leader_addr.encode()
+        else:
+            return FAILURE
 
     async def request_votes(self):
         while True:
@@ -196,44 +200,32 @@ class Node(object):
 
             self.voting = True
             self.current_term += 1
-            print("Start voting for new term", self.current_term)
+            LOGGER.info(f"Start voting for new term {self.current_term}")
 
             rpc = RequestVote(self.current_term, self.addr, self.logs[-1].index, self.logs[-1].term)
 
             num_votes = 1
             results = await asyncio.gather(*[peer.request_vote(rpc) for peer in self.peers], return_exceptions=True)
             for index, result in enumerate(results):
-                print(f"Peer at {self.peers[index].addr} {'voted for me' if result else 'NOT vote for me'}")
+                LOGGER.info(f"Peer at {self.peers[index].addr} {'voted for me' if result else 'NOT vote for me'}")
                 if result:
                     num_votes += 1
 
-            print(f"Cluster size: {len(self.peers) + 1}, quorum: {quorum}, votes: {num_votes}")
+            LOGGER.info(f"Cluster size: {len(self.peers) + 1}, quorum: {quorum}, votes: {num_votes}")
             if num_votes >= quorum:
-                print("I'm the Leader now!")
+                LOGGER.info("I'm the Leader now!")
+
                 self.leader_addr = self.addr
+                self.next_indices = [len(self.logs)] * len(self.peers)
+                self.match_indices = [len(self.logs) - 1] * len(self.peers)
+
+                await self.append_entries()
 
             self.voting = False
-            break
-
-    async def look_for_leader(self):
-        assert self.leader_addr is None
-
-        results = await asyncio.gather(*[peer.look_for_leader() for peer in self.peers], return_exceptions=True)
-        for index, result in enumerate(results):
-            if result is not None:
-                self.leader_addr = result
-                print(f"Get Leader at {result}")
-                break
-
-    def on_look_for_leader(self) -> bytes:
-        if self.leader_addr is not None:
-            return b'{"ok":true,"value":"%s"}' % self.leader_addr.encode()
-        else:
-            return FAILURE
 
     def on_request_vote(self, data: bytes) -> bytes:
         if self.voting:
-            print("I'm arranging an vote currently")
+            LOGGER.info("I'm arranging an vote currently")
             return FAILURE
 
         rpc = fromdict(RequestVote, json.loads(data))
@@ -247,20 +239,63 @@ class Node(object):
         if self.logs[-1].index > rpc.last_log_index or self.logs[-1].term > rpc.last_log_term:
             return FAILURE
 
-        print("Voted for", rpc.candidate_addr)
+        LOGGER.info(f"Voted for {rpc.candidate_addr}")
         self.voted_for = rpc.candidate_addr
 
         return b'{"ok":true,"term":%d}' % rpc.term
 
+    async def append_entries(self):
+        await asyncio.gather(
+            *[self.append_entries_for_peer(index) for index in range(len(self.peers))], return_exceptions=True)
+
+    async def append_entries_for_peer(self, peer_index: int):
+        next_index = self.next_indices[peer_index]
+        new_next_index = len(self.logs)
+        previous_entry = self.logs[self.match_indices[peer_index]]
+
+        rpc = AppendEntries(self.current_term, self.addr, self.commit_index,
+                            self.logs[next_index:], previous_entry.index, previous_entry.term)
+
+        response = await self.peers[peer_index].send("APPEND_ENTRIES " + util.dump_as_json(asdict(rpc)))
+        if response.rstrip() != FAILURE:
+            obj = json.loads(response)
+            if obj.get("ok") is True:
+                self.next_indices[peer_index] = new_next_index
+                self.match_indices[peer_index] = new_next_index - 1
+
+    def on_append_entries(self, data: bytes) -> bytes:
+        rpc = fromdict(AppendEntries, json.loads(data))
+
+        if self.current_term > rpc.term:
+            return FAILURE
+        if self.current_term < rpc.term:
+            self.current_term = rpc.term
+        if self.leader_addr != rpc.leader_addr:  # Switch to the new leader
+            self.leader_addr = rpc.leader_addr
+
+        if self.logs[-1].index != rpc.prev_log_index:
+            return FAILURE
+
+        if self.logs[-1].term != rpc.term and len(self.logs) > 1:
+            self.logs = self.logs[:-1]
+            return FAILURE
+
+        if len(rpc.entries) > 0:
+            self.logs += rpc.entries
+
+        return b'{"ok":true}'
+
     def run(self, message: bytes) -> bytes:
         if message == b"PING\r\n":
             return PONG
+        elif message.startswith(b"APPEND_ENTRIES "):
+            return self.on_append_entries(message[15:])
         elif message == b"LOOK_FOR_LEADER\r\n":
             return self.on_look_for_leader()
         elif message.startswith(b"REQUEST_VOTE "):
             return self.on_request_vote(message[13:])
 
-        return f"Response from {self.addr} for message `{message.rstrip()}`".encode()
+        return f"General response from {self.addr} for unknown message `{message.rstrip()}`".encode()
 
     async def handle_connection(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         addr = writer.get_extra_info("peername")
@@ -268,30 +303,34 @@ class Node(object):
         try:
             while True:
                 message = await reader.readline()
-                print(f"Received {message} from {addr}")
+                LOGGER.info(f"Received {message} from {addr}")
 
                 if not message or message.startswith(b"EXIT\r\n"):
-                    print("Bye bye")
+                    LOGGER.info("Bye bye")
                     break
 
                 data = self.run(message)
 
-                print(f"Send: {data}")
+                LOGGER.debug(f"Send: {data}")
                 writer.write(data)
                 writer.write(b"\r\n")
                 await writer.drain()
         except Exception:
-            print(f"!!!Exception occurred <peer: {addr}>!!!")
-            traceback.print_exc()
+            LOGGER.exception(f"!!!Exception occurred <peer: {addr}>!!!")
 
-        print("Close the connection")
+        LOGGER.debug("Close the connection")
         writer.close()
 
 
 async def main():
     if len(sys.argv) < 3:
-        print(f"Usage: {sys.argv[0]} <port> <peer1> [<peer2> [<peer3> [...]]]")
+        logging.error(f"Usage: {sys.argv[0]} <port> <peer1> [<peer2> [<peer3> [...]]]")
         return
+
+    logging.getLogger().setLevel(logging.WARNING)
+    LOGGER.propagate = False
+    LOGGER.handlers = util.create_logging_handlers()
+    LOGGER.setLevel(logging.DEBUG)
 
     global LOOP
     LOOP = asyncio.get_event_loop()
@@ -302,7 +341,7 @@ async def main():
     server = await asyncio.start_server(node.handle_connection, "localhost", port)
 
     addrs = ", ".join(str(sock.getsockname()) for sock in server.sockets)
-    print(f"Serving on {addrs}")
+    LOGGER.info(f"Serving on {addrs}")
 
     async with server:
         await asyncio.gather(node.post_initialize(), node.request_votes(), server.serve_forever())
---
+++ Python/util.py
@@ -0,0 +1,36 @@
+import json
+import logging
+import logging.handlers
+import sys
+
+
+def dump_as_json(d: dict):
+    return json.dumps(d, separators=(",", ":"))
+
+
+class ColorfulFormatter(logging.Formatter):
+    GREY = "\x1b[38;21m"
+    YELLOW = "\x1b[33;21m"
+    RED = "\x1b[31;21m"
+    BOLD_RED = "\x1b[31;1m"
+    RESET = "\x1b[0m"
+    FORMAT = "%(asctime)s [%(levelname)s] %(process)s %(module)s: %(message)s"
+
+    FORMATS = {
+        logging.DEBUG: GREY + FORMAT + RESET,
+        logging.INFO: GREY + FORMAT + RESET,
+        logging.WARNING: YELLOW + FORMAT + RESET,
+        logging.ERROR: RED + FORMAT + RESET,
+        logging.CRITICAL: BOLD_RED + FORMAT + RESET
+    }
+
+    def format(self, record: logging.LogRecord):
+        formatter = logging.Formatter(self.FORMATS.get(record.levelno, logging.INFO))
+        return formatter.format(record)
+
+
+def create_logging_handlers():
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(ColorfulFormatter())
+
+    return console_handler,
